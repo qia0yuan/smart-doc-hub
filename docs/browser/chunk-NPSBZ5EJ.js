80c6fca788f3493731fdb26b7dc6341f97cc1421
George Pu <george@george-lenovo>
2025-02-25 17:07:27 -0500

    022525

--- docs/browser/chunk-NPSBZ5EJ.js
+++
@@ -1,1 +0,0 @@
-import{$a as e,Mb as r,vb as s,wb as n}from"./chunk-IGD7WYMR.js";var a=class t{static \u0275fac=function(o){return new(o||t)};static \u0275cmp=e({type:t,selectors:[["app-forgot-password"]],decls:2,vars:0,template:function(o,i){o&1&&(s(0,"p"),r(1,"forgot-password works!"),n())},encapsulation:2})};export{a as ForgotPasswordComponent};
